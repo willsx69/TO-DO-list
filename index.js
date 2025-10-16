71f25b8529cd307f0f3f5337f8bd3d70250b45b0
william <williambreno666@gmail.com>
2025-10-15 20:22:26 -0400

    segundo commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes');
+
+
 const app = express();
 const port = 3000;
+
+
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 
 
-app.get ('/', (req , res) => {
-    res.render("index");
-});
 
 app.listen(port, () => {
     console.log('Server is running on port 3000');
